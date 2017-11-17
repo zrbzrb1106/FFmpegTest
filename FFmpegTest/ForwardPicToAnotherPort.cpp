7f4c158ee1281231d96b7798c60c391535b46601
Renbing Zhang <Renbing Zhang>
2017-11-17 20:08:45 +0100

    添加项目文件。

---
+++ FFmpegTest/ForwardPicToAnotherPort.cpp
@@ -0,0 +1,65 @@
+﻿#include "stdafx.h"
+#include <WinSock2.h>
+#include <iostream>
+#include <Ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")  //load ws2_32.dll
+
+using namespace std;
+
+class ForwardPicToAnotherPort {
+
+public:
+	WSADATA wsaData;
+	SOCKET sock;
+	sockaddr_in servAddr;
+	sockaddr fromAddr;
+	int addrLen;
+
+	
+
+	void init_Socket(void) {
+		WSAStartup(MAKEWORD(2, 2), &wsaData);
+		
+		sock = socket(PF_INET, SOCK_DGRAM, 0);
+		memset(&servAddr, 0, sizeof(servAddr));
+
+		servAddr.sin_family = PF_INET;
+		inet_pton(PF_INET, "127.0.0.1", &(servAddr.sin_addr));
+		servAddr.sin_port = htons(1234);
+
+		addrLen = sizeof(fromAddr);
+	}
+	
+	void sendPicData(AVFrame *pFrame, long int bufSize,
+		int width, int height, int iFrame) {
+
+		char *buffer;
+		buffer = new char[bufSize];
+		//Bytes number every line of picture
+		int lineBytes = pFrame->linesize[0];
+
+		memcpy(buffer, pFrame->data[0]+300* lineBytes, lineBytes);
+		// buffer!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		/*
+		for (int y = 0; y < height; y++) {
+			memcpy(&buffer[0 + y*pFrame->linesize[0]], 
+				pFrame->data[0+y*pFrame->linesize[0]], pFrame->linesize[0]);
+		}
+		*/
+
+		cout << pFrame->data[0] << endl;
+
+		//cout << pFrame->data[0] << endl;
+		sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr*)&servAddr, sizeof(servAddr));
+		buffer = {0};
+		//cout << "Message form server: " << buffer << endl;
+		
+		
+	}
+
+	void end_Socket() {
+		closesocket(sock);
+		WSACleanup();
+	}
+
+};
---
+++ FFmpegTest/IncludeFFmpeg.h
@@ -0,0 +1,7 @@
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
