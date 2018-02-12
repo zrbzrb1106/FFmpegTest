7eba2ec7be12a81dc472799fc3e33facde95cf1d
任炳 张 <renbing_z@outlook.com>
2018-02-12 15:21:27 +0100

    Change

--- FFmpegTest/IncludeFFmpeg.h
+++ FFmpegTest/IncludeFFmpeg.h
@@ -4,4 +4,8 @@ extern "C"
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
+#include <libavfilter/buffersrc.h>
+#include <libavfilter/buffersink.h>
+#include <libavutil/avassert.h>
+#include <libavutil/imgutils.h>
 }
---
+++ FFmpegTest/IncludeOpencv.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/shape_utils.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2\core\ocl.hpp>
